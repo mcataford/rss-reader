6e10a2bcde59bc92bf95811f4a4599f57ef75645
Marc Cataford <mcat@riseup.net>
2021-07-14 23:39:48 -0400

    feat: settings panel

--- src/SettingsPanel.tsx
+++ src/SettingsPanel.tsx
@@ -1,6 +1,12 @@
-import React, { ReactNode } from 'react'
+import React, { ReactNode, useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import Card from '@material-ui/core/Card'
 import TextField from '@material-ui/core/TextField'
 import Button from '@material-ui/core/Button'
+import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline'
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline'
 
 import { persistSettings } from './utils/persistence'
 
@@ -9,22 +15,69 @@ interface Props {
     setFeedUrls: (s: string[]) => void
 }
 
+const useStyles = makeStyles({
+    urlCard: {
+        margin: '5px',
+        padding: '5px',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '5px',
+    },
+})
+
+function isValidUrl(url: string): boolean {
+    const urlPattern = /(https?:\/\/)?(www\.)?[\w.-_]+\.[a-zA-Z]{2,3}/
+
+    return urlPattern.test(url)
+}
+
 export default function SettingsPanel(props: Props): ReactNode {
     const { feedUrls, setFeedUrls } = props
+    const [feedUrlsForm, setFeedUrlsForm] = useState(feedUrls)
+
+    const classes = useStyles()
+    const urlCards = feedUrlsForm.map((url) => (
+        <Card key={`url_${url}`} variant="outlined" className={classes.urlCard}>
+            {isValidUrl(url) ? (
+                <CheckCircleOutlineIcon color="primary" />
+            ) : (
+                <ErrorOutlineIcon color="error" />
+            )}{' '}
+            {url}
+        </Card>
+    ))
 
     return (
-        <>
+        <Box display="flex" flexDirection="column">
+            <Typography paragraph>
+                {
+                    'Enter URLs to fetch feeds from. Invalid URLs are discarded on save.'
+                }
+            </Typography>
             <TextField
                 multiline
                 rows={5}
                 variant="outlined"
                 label="Feed URLs"
-                defaultValue={feedUrls.join('\n')}
-                onChange={(v) => setFeedUrls(v.target.value.split('\n'))}
+                fullWidth
+                value={feedUrlsForm.join('\n')}
+                onChange={(v) => setFeedUrlsForm(v.target.value.split('\n'))}
             />
-            <Button onClick={() => persistSettings({ feedUrls: feedUrls })}>
+            <Box display="flex" flexDirection="column">
+                {urlCards}
+            </Box>
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                    const validUrls = feedUrlsForm.filter(isValidUrl)
+                    setFeedUrls(validUrls)
+                    setFeedUrlsForm(validUrls)
+                    persistSettings({ feedUrls: validUrls })
+                }}
+            >
                 Save
             </Button>
-        </>
+        </Box>
     )
 }
